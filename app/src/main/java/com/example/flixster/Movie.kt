ad296fde89ee552efaedd0fe0c383c9dd123962b
Elias0305Ha <erdunoelias@gmail.com>
2022-03-04 20:57:47 -0600

    Exposing details of movie and Allowing video posts to be played in full-screen using the YouTubePlayerView

--- app/src/main/java/com/example/flixster/Movie.kt
+++ app/src/main/java/com/example/flixster/Movie.kt
@@ -1,17 +1,24 @@
 package com.example.flixster
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.IgnoredOnParcel
 import org.json.JSONArray
-
+@Parcelize
 data class Movie (
 
     val movieId: Int,
+    val voteAverage: Double,
     private val posterPath: String,
     val title: String,
     val overView: String,
 
 
-){
+)  : Parcelable {
+    @IgnoredOnParcel
     val posterImageUrl = "https://image.tmdb.org/t/p/w342/$posterPath"
+
+
     companion object{
 
         fun fromJsonArray(movieJsonArray: JSONArray):List<Movie> {
@@ -21,6 +28,7 @@ data class Movie (
                 movies.add(
                     Movie(
                         movieJson.getInt("id"),
+                        movieJson.getDouble("vote_average"),
                         movieJson.getString("poster_path"),
                         movieJson.getString("title"),
                         movieJson.getString("overview")
--- app/src/main/java/com/example/flixster/MovieAdapter.kt
+++ app/src/main/java/com/example/flixster/MovieAdapter.kt
@@ -1,16 +1,18 @@
 package com.example.flixster
 
 import android.content.Context
+import android.content.Intent
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 
-
+const val MOVIE_EXTRA = "MOVIE_EXTRA"
 private const val TAG = "MovieAdapter"
 class MovieAdapter(private val context: Context, private val movies: MutableList<Movie>)
     : RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
@@ -30,11 +32,16 @@ class MovieAdapter(private val context: Context, private val movies: MutableList
     }
 
     override fun getItemCount() = movies.size
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
 
         private val ivPoster = itemView.findViewById<ImageView>(R.id.ivPoster)
         private val tvTitle = itemView.findViewById<TextView>(R.id.tvTitle)
         private val tvOverview = itemView.findViewById<TextView>(R.id.tvOverview)
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
 
         fun bind(movie:Movie){
 
@@ -43,6 +50,19 @@ class MovieAdapter(private val context: Context, private val movies: MutableList
             Glide.with(context).load(movie.posterImageUrl).into(ivPoster)
 
 
+
+        }
+
+        override fun onClick(p0: View?) {
+            // 1. get notified of particular movie which is taped on
+            val movie= movies[adapterPosition]
+            Toast.makeText(context, movie.title, Toast.LENGTH_SHORT).show()
+
+            // 2. use the intent to navigate to the new activity
+            val intent = Intent(context, DetailActivity::class.java)
+            intent.putExtra("movie_title", movie.title)
+            intent.putExtra(MOVIE_EXTRA, movie)
+            context.startActivity(intent)
 
         }
 
